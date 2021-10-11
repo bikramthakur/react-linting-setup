999d2cfb55a0e767705821eb1afbe49e60e88ade
Bikram Thakur <your_email@example.com>
2021-10-11 18:31:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const hello = 'Hello'
+  debugger
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
